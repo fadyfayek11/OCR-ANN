436df40b4f41610b0237b30ae077b5b325c8d6ed
fadyfayek11 <fayekfady24@gmail.com>
2021-03-18 18:11:05 +0200

    uncomment

--- main.py
+++ main.py
@@ -18,53 +18,53 @@ data_train = data_1.copy()
 train_lable = data_train.values[:, 0]
 train_letter = data_train.values[:, 1:]
 
+
 # preprocessing
 
-# def rotate(image):
-#     image = image.reshape([28, 28])
-#     image = num.fliplr(image)
-#     image = num.rot90(image)
-#     return image
-#
-#
-# train_letter = num.asarray(train_letter)
-# train_letter = num.apply_along_axis(rotate, 1, train_letter)
-#
-# test_letter = num.asarray(test_letter)
-# test_letter = num.apply_along_axis(rotate, 1, test_letter)
-#
-#
-# test_letter = tf.keras.utils.normalize(test_letter, axis=1)
-# train_letter = tf.keras.utils.normalize(train_letter, axis=1)
+def rotate(image):
+    image = image.reshape([28, 28])
+    image = num.fliplr(image)
+    image = num.rot90(image)
+    return image
+
+
+train_letter = num.asarray(train_letter)
+train_letter = num.apply_along_axis(rotate, 1, train_letter)
+
+test_letter = num.asarray(test_letter)
+test_letter = num.apply_along_axis(rotate, 1, test_letter)
+
+test_letter = tf.keras.utils.normalize(test_letter, axis=1)
+train_letter = tf.keras.utils.normalize(train_letter, axis=1)
+
 ##############################
 
 # build our model
 
-# model = tf.keras.models.Sequential()
+model = tf.keras.models.Sequential()
 
-# model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Flatten())
 
-# model.add(tf.keras.layers.Dense(units=1024, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
-# model.add(tf.keras.layers.Dense(units=256, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=1024, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=512, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=256, activation=tf.nn.relu))
 
 
-# model.add(tf.keras.layers.Dense(units=27, activation=tf.nn.softmax))
+model.add(tf.keras.layers.Dense(units=27, activation=tf.nn.softmax))
 
 #######compile our model########
 
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# model.fit(train_letter, train_lable, epochs=7)
-
+model.fit(train_letter, train_lable, epochs=7)
 
 
 ######predict########
 
-# loss, accuracy = model.evaluate(test_letter, test_lable)
-# print(accuracy)
-# print(loss)
-# model.save('digits.model1')
+loss, accuracy = model.evaluate(test_letter, test_lable)
+print(accuracy)
+print(loss)
+model.save('digits.model1')
 
 
 model1 = tf.keras.models.load_model('digits.model')
